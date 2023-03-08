35f26eaf6d60fd9b5757ac6905f9c2dcd24335b7
LuanPonick <luaponick.sz.2@gmail.com>
2023-03-08 14:35:30 -0300

    create project

---
+++ src/main/java/com/AlugaMeCar/AlugaMeCar/AlugaMeCarApplication.java
@@ -0,0 +1,13 @@
+package com.AlugaMeCar.AlugaMeCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaMeCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaMeCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AlugaMeCar/AlugaMeCar/AlugaMeCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlugaMeCar.AlugaMeCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaMeCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
