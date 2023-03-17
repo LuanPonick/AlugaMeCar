902baaf4254ee07275fc9b30c2310a2ab8d799d8
LuanPonick <luanponick.sz.2@gmail.com>
2023-03-17 13:54:26 -0300

    update

---
+++ src/main/java/com/AlugaMeCar/AlugaMeCar/repository/CarrpRepository.java
@@ -0,0 +1,8 @@
+package com.AlugaMeCar.AlugaMeCar.repository;
+
+import com.AlugaMeCar.AlugaMeCar.model.Carro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarrpRepository extends JpaRepository<Carro,Long> {
+
+}
--- src/main/java/com/AlugaMeCar/AlugaMeCar/repository/ClienteRepository.java
+++ src/main/java/com/AlugaMeCar/AlugaMeCar/repository/ClienteRepository.java
@@ -1,4 +1,4 @@
-package com.AlugaMeCar.AlugaMeCar.Repository;
+package com.AlugaMeCar.AlugaMeCar.repository;
 
 import com.AlugaMeCar.AlugaMeCar.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
