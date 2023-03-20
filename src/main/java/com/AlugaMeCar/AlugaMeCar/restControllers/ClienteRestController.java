73ffa4b24b72a310ce505efc8f434a232216fabf
Brenospf11 <brenospf7@gmail.com>
2023-03-20 16:06:07 -0300

    get, post and delete

--- src/main/java/com/AlugaMeCar/AlugaMeCar/restControllers/ClienteRestController.java
+++ src/main/java/com/AlugaMeCar/AlugaMeCar/restControllers/ClienteRestController.java
@@ -4,6 +4,7 @@ import com.AlugaMeCar.AlugaMeCar.dto.ClienteDTO;
 import com.AlugaMeCar.AlugaMeCar.model.Cliente;
 import com.AlugaMeCar.AlugaMeCar.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,16 @@ public class ClienteRestController {
     }
 
     @PostMapping(value = "/")
-    public ResponseEntity<Cliente> save(@RequestBody Cliente entity){
+    public ResponseEntity<Cliente> saveById(@RequestBody Cliente entity){
         return clienteService.save(entity);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Cliente> deleteById(@PathVariable("id") Long id){
+        return clienteService.deleteById(id);
+    }
+
+//    @PutMapping(value = "/{id}")
+//    public ResponseEntity<Cliente> alterById( @RequestBody Cliente c){
+//    }
 }
---
+++ src/main/java/com/AlugaMeCar/AlugaMeCar/restControllers/EnderecoRestController.java
@@ -0,0 +1,30 @@
+package com.AlugaMeCar.AlugaMeCar.restControllers;
+
+import com.AlugaMeCar.AlugaMeCar.dto.EnderecoDTO;
+import com.AlugaMeCar.AlugaMeCar.model.Endereco;
+import com.AlugaMeCar.AlugaMeCar.services.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/endereco")
+public class EnderecoRestController {
+//    @Autowired
+//    private EnderecoService enderecoService;
+//
+//    @GetMapping(value = "/{id}")
+//    public EnderecoDTO findById(@PathVariable Long id){
+//        return enderecoService.getById(id);
+//    }
+//
+//    @PostMapping(value = "/")
+//    public ResponseEntity<Endereco> saveById(@RequestBody Endereco entity){
+//        return enderecoService.save(entity);
+//    }
+//
+//    @DeleteMapping(value = "/{id}")
+//    public ResponseEntity<Endereco> deleteById(@PathVariable("id") Long id){
+//        return enderecoService.deleteById(id);
+//    }
+}
